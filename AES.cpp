555f70998ed2727afdb67e8c1eec1a98d57a736e
Fauxmen4 <77955560+Fauxmen4@users.noreply.github.com>
2023-03-14 02:20:29 +0300

    Add files via upload

---
+++ AES.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <md5.h>
+
+using namespace std;
+
+// string encrypt(string block, string key) {
+
+// }
+
+// string decrypt(string cipher, string key) {
+
+// }
+
+int main() {
+    int a, b = 65, c = 83;
+    a = b ^ c;
+    cout << a << '\n';
+}
---
+++ Hash.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string HashFunctionASCII(string input) {
+    unsigned long long hashValue = 1;
+    for (auto i = input.begin(); i != input.end(); i++) {
+        hashValue *= (int)(*i);
+    }
+    return to_string(hashValue);
+}
+
+string HashFunctionStatistics(string input) {
+    string hashValue = "";
+    int *charStatistics = new int[128];
+    for (auto i = input.begin(); i != input.end(); i++) {
+        charStatistics[(int)(*i)]++;
+    }
+    for (int i = 0; i < 128; i++) {
+        if (charStatistics[i] != 0) {
+            hashValue.append(to_string(charStatistics[i]));
+        }
+    }
+    return hashValue;
+}
+
+int main() {
+    ifstream fin("./Example.txt");
+
+    //! Warning!!! Just an example.
+    string str;
+    if (!fin.is_open()) {return 1;}
+
+    while(getline(fin, str)) {
+        cout << str << '\n';
+    }
+    
+
+    fin.close();
+    return 0;
+}
